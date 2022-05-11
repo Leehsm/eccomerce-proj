1c6cabc1a6704eda185b1d2448830c2af62ccc90
Leehsm <shahzleehasimwork@gmail.com>
2022-05-11 08:50:42 +0800

    blog frontend

---
+++ resources/views/frontend/others/blogDetails.blade.php
@@ -0,0 +1,49 @@
+@extends('frontend.main_master')
+@section('content')
+
+@section('title')
+Blog
+@endsection
+
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="{{ url('/') }}">Home</a></li>
+				<li class='active'>Blog</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
+
+<div class="body-content">
+	<div class="container">
+		<div class="row">
+			<div class="blog-page">
+				<div class="col-md-12">
+					<div class="blog-post wow fadeInUp">
+                        <img class="img-responsive" src="{{ asset($blogs->blogImg) }}" style="display: block; margin-left: auto; margin-right: auto;">
+                        <h1>{{ $blogs->title }}</h1>
+                        <span class="date-time">{{ $blogs->date }}</span>
+                        <p style="text-align: justify; text-justify: inter-word;">{{ $blogs->long_description }}</p>                       
+                        <div class="social-media">
+                            <span>share post:</span>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-whatsapp"></i></a>
+                        </div>
+                    </div>
+				</div>		
+            </div>
+        </div>
+    </div>
+</div>
+		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+    <div class="logo-slider-inner">	
+        <div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+            
+        </div><!-- /.owl-carousel #logo-slider -->
+    </div><!-- /.logo-slider-inner -->
+</div><!-- /.logo-slider -->
+@endsection
