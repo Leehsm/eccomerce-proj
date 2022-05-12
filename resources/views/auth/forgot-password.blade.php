46d5682bdbed53b8f7d569d08c9c1d6425153b3e
Leehsm <shahzleehasimwork@gmail.com>
2022-05-12 12:05:02 +0800

    register & login & forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -17,22 +17,23 @@
 		<div class="sign-in-page">
 			<div class="row">
 				<!-- Sign-in -->			
-<div class="col-md-6 col-sm-6 sign-in">
-	<h4 class="">Forget Password</h4>
-	<p class="">Forgot your password? No Problem</p>
-	
-	<form method="POST" action="{{ route('password.email') }}">
-        @csrf
-		<div class="form-group">
-		    <label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
-		    <input type="email" id="email" name="email" class="form-control unicase-form-control text-input">
+				<div class="col-md-6 col-sm-6 sign-in">
+					<h4 class="">Forget Password</h4>
+					<p class="">Forgot your password? No Problem</p>
+					
+					<form method="POST" action="{{ route('password.email') }}">
+						@csrf
+						<div class="form-group">
+							<label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
+							<input type="email" id="email" name="email" class="form-control unicase-form-control text-input">
+						</div>
+						
+						<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Email Password Reset Link</button>
+					</form>					
+				</div>
+			</div>
 		</div>
-		
-	  	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Email Password Reset Link</button>
-	</form>					
-</div>
-<!-- Sign-in -->
-</div><!-- /.sigin-in-->
+	</div>
         
 @include('frontend.body.brand')
 </div><!-- /.body-content -->
